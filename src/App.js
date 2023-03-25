e9553c0bf9518818724ce662d021012fded007b3
Henri Daniel Peña Dequero <henridanielpenadequero@gmail.com>
2023-03-25 11:00:53 -0400

    feat: Started coding app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Carpintería Ahmed</h1>
       </header>
+      <section>
+        <h2>Productos de calidad hecho a medida</h2>
+        <p>Se ofrece los mejores servicios de carpintería a pedido</p>
+      </section>
     </div>
   );
 }
